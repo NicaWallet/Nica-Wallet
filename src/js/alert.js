2bd6cbab14cd3f691780b2fb045a55362c0463cd
Engels Fuentes <engelsfuentes13@gmail.com>
2024-03-30 19:26:05 -0600

    First advances layout register user

---
+++ src/js/alert.js
@@ -0,0 +1,8 @@
+document.getElementById('miBoton').addEventListener('click', function() {
+    Swal.fire({
+        title: 'Registrado',
+        text: 'Bienvenido a Nicawallet',
+        icon: 'info',
+        confirmButtonText: 'Entendido'
+    });
+});
